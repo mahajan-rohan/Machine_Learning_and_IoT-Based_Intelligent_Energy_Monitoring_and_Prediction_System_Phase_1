4aceeda98ab9121f8f3496917e66dbfee07eb461
Rohan Vikas Mahajan <125277825+mahajan-rohan@users.noreply.github.com>
2024-11-19 09:50:12 +0530

    Update firebase_pzem_Serial_RTC_SD_temp_relay_esp.ino

--- firebase_pzem_Serial_RTC_SD_temp_relay_esp/firebase_pzem_Serial_RTC_SD_temp_relay_esp.ino
+++ firebase_pzem_Serial_RTC_SD_temp_relay_esp/firebase_pzem_Serial_RTC_SD_temp_relay_esp.ino
@@ -10,7 +10,7 @@
 #include "addons/RTDBHelper.h"
 
 // WiFi and Firebase Configurations
-#define API_KEY "AIzaSyBaGCN2sjd9JSyosAj0EJFqbKPb8Y-13lY"
+#define API_KEY "AIzaSyBaGCN2sjd9JSyosAj0EJFqbKPb8Y-13l"
 #define DATABASE_URL "http://relay-cf260-default-rtdb.firebaseio.com/"
 #define WIFI_SSID "Mahajan"
 #define WIFI_PASSWORD "#Rohan@25&"
